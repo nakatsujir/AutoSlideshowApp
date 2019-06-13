f86aeb679ebbeb50e6fded517c1e24515607cb81
nakatsuji <rie.nakatsuji1123@gmail.com>
2019-06-13 21:18:15 +0900

    スライドショー機能追加

--- app/src/main/java/jp/techacademy/rie/ijichi/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/rie/ijichi/autoslideshowapp/MainActivity.kt
@@ -6,56 +6,142 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
 import android.content.ContentUris
+import android.content.DialogInterface
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
 import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_CODE_PERMISSION = 100
+    private var imageCursor: Cursor? = null
+    private var mTimer: Timer? = null
+    private var mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                getImage()
+        stop_button.visibility = View.GONE
 
-            }else{
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),REQUEST_CODE_PERMISSION)
-            }
-        }else{
-            getImage()
-        }
+        showPermission()
 
         next_button.setOnClickListener {
-            getImage()
+            if (imageCursor!!.isLast) {
+                getImage(2)
+            } else {
+                getImage(0)
+            }
+        }
+
+        back_button.setOnClickListener {
+            if (imageCursor!!.isFirst) {
+                getImage(3)
+            } else {
+                getImage(1)
+            }
+        }
+
+        move_button.setOnClickListener {
+            stop_button.visibility = View.VISIBLE
+            move_button.visibility = View.GONE
+            next_button.visibility = View.GONE
+            back_button.visibility = View.GONE
+            showSlide()
+        }
+
+        stop_button.setOnClickListener {
+            stopSlide()
+            stop_button.visibility = View.GONE
+            move_button.visibility = View.VISIBLE
+            next_button.visibility = View.VISIBLE
+            back_button.visibility = View.VISIBLE
+        }
+
+    }
+
+    private fun showPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getImage(2)
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_PERMISSION)
+            }
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
+        when (requestCode) {
             REQUEST_CODE_PERMISSION ->
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    //許可ボタンおされた時にしたい処理
-                    getImage()
-                }else{
-                    //許可されなかった
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getImage(2)
+                } else {
+                    AlertDialog.Builder(this).apply {
+                        setMessage("スライドを開始するにはパーミッション許可が必要です。")
+                        setPositiveButton("OK") { _, _ ->
+                            showPermission()
+                        }
+                        show()
+                    }
                 }
         }
     }
 
-    private fun getImage(){
-        contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null)?.let {
-            if (it.moveToNext()){
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,it.getLong(it.getColumnIndex(MediaStore.Images.Media._ID)))
+    private fun getImage(isOperation: Int) {
+        contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null)?.let {
+            if (imageCursor == null)
+                imageCursor = it
+
+            val cursorMove = when (isOperation) {
+                0 -> imageCursor!!.moveToNext()
+                1 -> imageCursor!!.moveToPrevious()
+                2 -> imageCursor!!.moveToFirst()
+                3 -> imageCursor!!.moveToLast()
+                else -> false
+            }
+            if (cursorMove) {
+                val imageUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    imageCursor!!.getLong(it.getColumnIndex(MediaStore.Images.Media._ID))
+                )
                 Log.d("ANDROID", "URI : " + imageUri.toString())
                 imageView.setImageURI(imageUri)
+                val position = imageCursor!!.position
+                Log.d("ANDROID", "$position")
             }
-            it.close()
         }
     }
 
+    private fun showSlide() {
+        if (mTimer == null) {
+            mTimer = Timer()
+            mTimer!!.schedule(object : TimerTask() {
+                override fun run() {
+                    mHandler.post {
+                        getImage(0)
+                    }
+                }
+            }, 2000, 2000)
+        }
+    }
 
+    private fun stopSlide() {
+        if (mTimer != null) {
+            mTimer!!.cancel()
+            mTimer = null
+        }
+        if (imageCursor!!.isAfterLast) {
+            getImage(3)
+        }
+    }
 }
+
+
+
